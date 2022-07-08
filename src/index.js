2a8ae72f308123aa2a23133ff62fffdfa18198b1
LucasSousa09 <lucasrodrigues.sousa09@gmail.com>
2022-07-08 16:59:36 -0300

    2nd Challenge Complete - Working with middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,73 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: "User not found"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if(!user.pro){
+    if(user.todos.length >= 10){
+      return response.status(403).json({error: "Reached the maximum number of todos for free account"})
+    }
+
+    return next()
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const validatedTodo = validate(id)
+
+  if(validatedTodo === false){
+    return response.status(400).json({error: "Invalid Todo!"})
+  }
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: "User not found"})
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({error: "Todo not found!"})
+  }
+
+  request.user = user
+  request.todo = todo
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if(!user){
+    return response.status(404).json({error: "User not found!"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
